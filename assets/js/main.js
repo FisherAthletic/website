8bd77c2c0538d62b0969f722a05f87ace4377385
Yobany Perez <Yobany.Perez@fisher.com>
2019-04-09 13:10:09 -0400

    Transitioned to a bootstrap navbar.

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon
-function addResponsive() {
-    var x = document.getElementById("topNavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
