a516db9ac52e4af5da9f3755a8231ac384f6722d
Perez <Yobany.Perez@local.fisherathletic.com>
2019-05-21 11:14:43 -0400

    JS code is now more effecient.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,44 @@
-function tackleWheelOption28 () {
-    document.getElementById("productDetailsImage").src = "../assets/images/football/tw2814.jpg";
-    document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW2814";
-    document.getElementById("tackleWheelSpecs").innerHTML = '28" Outside Diam. x 14" Inside Diam. 8" thick | 21 lbs | Rebond Foam';
-    document.getElementById("tackleWheelPrice").innerHTML = "$152.00";
-}
+// function tackleWheelOption28 () {
+//     document.getElementById("productDetailsImage").src = "../assets/images/football/tw2814.jpg";
+//     document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW2814";
+//     document.getElementById("tackleWheelSpecs").innerHTML = '28" Outside Diam. x 14" Inside Diam. 8" thick | 21 lbs | Rebond Foam';
+//     document.getElementById("tackleWheelPrice").innerHTML = "$152.00";
+// }
 
-function tackleWheelOption36 () {
-    document.getElementById("productDetailsImage").src = "../assets/images/football/tw3616.jpg";
-    document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW3616";
-    document.getElementById("tackleWheelSpecs").innerHTML = '36" Outside Diam. x 16" Inside Diam. 8" thick | 32 lbs | Rebond Foam';
-    document.getElementById("tackleWheelPrice").innerHTML = "$249.00";
-}
+// function tackleWheelOption36 () {
+//     document.getElementById("productDetailsImage").src = "../assets/images/football/tw3616.jpg";
+//     document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW3616";
+//     document.getElementById("tackleWheelSpecs").innerHTML = '36" Outside Diam. x 16" Inside Diam. 8" thick | 32 lbs | Rebond Foam';
+//     document.getElementById("tackleWheelPrice").innerHTML = "$249.00";
+// }
 
-function tackleWheelOption42 () {
-    document.getElementById("productDetailsImage").src = "../assets/images/football/tw4220.jpg";
-    document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW4220";
-    document.getElementById("tackleWheelSpecs").innerHTML = '42" Outside Diam. x 20" Inside Diam. 9" thick | 15 lbs | PolyFoam';
-    document.getElementById("tackleWheelPrice").innerHTML = "$325.00";
-}
+// function tackleWheelOption42 () {
+//     document.getElementById("productDetailsImage").src = "../assets/images/football/tw4220.jpg";
+//     document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW4220";
+//     document.getElementById("tackleWheelSpecs").innerHTML = '42" Outside Diam. x 20" Inside Diam. 9" thick | 15 lbs | PolyFoam';
+//     document.getElementById("tackleWheelPrice").innerHTML = "$325.00";
+// }
 
-function tackleWheelOption48 () {
-    document.getElementById("productDetailsImage").src = "../assets/images/football/tw4825.jpg";
-    document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW4825";
-    document.getElementById("tackleWheelSpecs").innerHTML = '48" Outside Diam. x 25" Inside Diam. 11" thick | 21 lbs | PolyFoam';
-    document.getElementById("tackleWheelPrice").innerHTML = "$357.00";
-}
+// function tackleWheelOption48 () {
+//     document.getElementById("productDetailsImage").src = "../assets/images/football/tw4825.jpg";
+//     document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW4825";
+//     document.getElementById("tackleWheelSpecs").innerHTML = '48" Outside Diam. x 25" Inside Diam. 11" thick | 21 lbs | PolyFoam';
+//     document.getElementById("tackleWheelPrice").innerHTML = "$357.00";
+// }
 
-function tackleWheelOption54 () {
-    document.getElementById("productDetailsImage").src = "../assets/images/football/tw5428.jpg";
-    document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW5428";
-    document.getElementById("tackleWheelSpecs").innerHTML = '54" Outside Diam. x 28" Inside Diam. 12" thick | 24 lbs | PolyFoam';
-    document.getElementById("tackleWheelPrice").innerHTML = "$407.00";
-}
+// function tackleWheelOption54 () {
+//     document.getElementById("productDetailsImage").src = "../assets/images/football/tw5428.jpg";
+//     document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW5428";
+//     document.getElementById("tackleWheelSpecs").innerHTML = '54" Outside Diam. x 28" Inside Diam. 12" thick | 24 lbs | PolyFoam';
+//     document.getElementById("tackleWheelPrice").innerHTML = "$407.00";
+// }
 
-function tackleWheelOption60 () {
-    document.getElementById("productDetailsImage").src = "../assets/images/football/tw6030.jpg";
-    document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW6030";
-    document.getElementById("tackleWheelSpecs").innerHTML = '60" Outside Diam. x 30" Inside Diam. 12" thick | 27 lbs | PolyFoam';
-    document.getElementById("tackleWheelPrice").innerHTML = "$425.00";
-}
+// function tackleWheelOption60 () {
+//     document.getElementById("productDetailsImage").src = "../assets/images/football/tw6030.jpg";
+//     document.getElementById("tackleWheelTitle").innerHTML = "Tackle Wheel TW6030";
+//     document.getElementById("tackleWheelSpecs").innerHTML = '60" Outside Diam. x 30" Inside Diam. 12" thick | 27 lbs | PolyFoam';
+//     document.getElementById("tackleWheelPrice").innerHTML = "$425.00";
+// }
 
 // Click functions that link to pages //
 
@@ -68,147 +68,99 @@ function windscreenGridClick () {
 
 // Baseball captions p1 //
 function msdpCaption () {
-  var msdpImage = document.getElementById("msdpImage");
-  var msdpCaption = document.getElementById("msdpCaption");
-    msdpCaption.innerHTML = msdpImage.alt;
-};
+  document.getElementById("msdpCaption").innerHTML = document.getElementById("msdpImage").alt;
+}
 
 function tmwsxxCaption () {
-  var tmwsxxImage = document.getElementById("tmwsxxImage");
-  var tmwsxxCaption = document.getElementById("tmwsxxCaption");
-    tmwsxxCaption.innerHTML = tmwsxxImage.alt;
+  document.getElementById("tmwsxxCaption").innerHTML = document.getElementById("tmwsxxImage").alt;
 };
 
 function dmwsCaption () {
-  var dmwsImage = document.getElementById("dmwsImage");
-  var dmwsCaption = document.getElementById("dmwsCaption");
-    dmwsCaption.innerHTML = dmwsImage.alt;
+  document.getElementById("dmwsCaption").innerHTML = document.getElementById("dmwsImage").alt;
 };
 
 function dbpsoCaption () {
-  var dbpsoImage = document.getElementById("dbpsoImage");
-  var dbpsoCaption = document.getElementById("dbpsoCaption");
-    dbpsoCaption.innerHTML = dbpsoImage.alt;
+  document.getElementById("dbpsoCaption").innerHTML = document.getElementById("dbpsoImage").alt;
 };
 
 function pcaps223Caption () {
-  var pcaps223Image = document.getElementById("pcaps223Image");
-  var pcaps223Caption = document.getElementById("pcaps223Caption");
-    pcaps223Caption.innerHTML = pcaps223Image.alt;
+  document.getElementById("pcaps223Caption").innerHTML = document.getElementById("pcaps223Image").alt;
 };
 
 function rpg184Caption () {
-  var rpg184Image = document.getElementById("rpg184Image");
-  var rpg184Caption = document.getElementById("rpg184Caption");
-    rpg184Caption.innerHTML = rpg184Image.alt;
+  document.getElementById("rpg184Caption").innerHTML = document.getElementById("rpg184Image").alt;
 };
 
 function rpsg184Caption () {
-  var rpsg184Image = document.getElementById("rpsg184Image");
-  var rpsg184Caption = document.getElementById("rpsg184Caption");
-    rpsg184Caption.innerHTML = rpsg184Image.alt;
+  document.getElementById("rpsg184Caption").innerHTML = document.getElementById("rpsg184Image").alt;
 };
 
 function bspCaption () {
-  var bspImage = document.getElementById("bspImage");
-  var bspCaption = document.getElementById("bspCaption");
-    bspCaption.innerHTML = bspImage.alt;
+  document.getElementById("bspCaption").innerHTML = document.getElementById("bspImage").alt;
 };
 
 function bsp46Caption () {
-  var bsp46Image = document.getElementById("bsp46Image");
-  var bsp46Caption = document.getElementById("bsp46Caption");
-    bsp46Caption.innerHTML = bsp46Image.alt;
+  document.getElementById("bsp46Caption").innerHTML = document.getElementById("bsp46Image").alt;
 };
 
 function bbc100Caption () {
-  var bbc100Image = document.getElementById("bbc100Image");
-  var bbc100Caption = document.getElementById("bbc100Caption");
-    bbc100Caption.innerHTML = bbc100Image.alt;
+  document.getElementById("bbc100Caption").innerHTML = document.getElementById("bbc100Image").alt;
 };
 
 function bfc250Caption () {
-  var bfc250Image = document.getElementById("bfc250Image");
-  var bfc250Caption = document.getElementById("bfc250Caption");
-    bfc250Caption.innerHTML = bfc250Image.alt;
+  document.getElementById("bfc250Caption").innerHTML = document.getElementById("bfc250Image").alt;
 };
 
 function trpg10Caption () {
-  var trpg10Image = document.getElementById("trpg10Image");
-  var trpg10Caption = document.getElementById("trpg10Caption");
-    trpg10Caption.innerHTML = trpg10Image.alt;
+  document.getElementById("trpg10Caption").innerHTML = document.getElementById("trpg10Image").alt;
 };
 
 
 // Baseball captions p2 //
 function bbc100gCaption () {
-  var bbc100gImage = document.getElementById("bbc100gImage");
-  var bbc100gCaption = document.getElementById("bbc100gCaption");
-    bbc100gCaption.innerHTML = bbc100gImage.alt;
+  document.getElementById("bbc100gCaption").innerHTML = document.getElementById("bbc100gImage").alt;
 };
 
 function pc1Caption () {
-  var pc1Image = document.getElementById("pc1Image");
-  var pc1Caption = document.getElementById("pc1Caption");
-    pc1Caption.innerHTML = pc1Image.alt;
+  document.getElementById("pc1Caption").innerHTML = document.getElementById("pc1Image").alt;
 };
 
 function tpt201550Caption () {
-  var tpt201550Image = document.getElementById("tpt201550Image");
-  var tpt201550Caption = document.getElementById("tpt201550Caption");
-    tpt201550Caption.innerHTML = tpt201550Image.alt;
+  document.getElementById("tpt201550Caption").innerHTML = document.getElementById("tpt201550Image").alt;
 };
 
 function hpm712Caption () {
-  var hpm712Image = document.getElementById("hpm712Image");
-  var hpm712Caption = document.getElementById("hpm712Caption");
-    hpm712Caption.innerHTML = hpm712Image.alt;
+  document.getElementById("hpm712Caption").innerHTML = document.getElementById("hpm712Image").alt;
 };
 
 function odc4Caption () {
-  var odc4Image = document.getElementById("odc4Image");
-  var odc4Caption = document.getElementById("odc4Caption");
-    odc4Caption.innerHTML = odc4Image.alt;
+  document.getElementById("odc4Caption").innerHTML = document.getElementById("odc4Image").alt;
 };
 
 function odc6Caption () {
-  var odc6Image = document.getElementById("odc6Image");
-  var odc6Caption = document.getElementById("odc6Caption");
-    odc6Caption.innerHTML = odc6Image.alt;
+  document.getElementById("odc6Caption").innerHTML = document.getElementById("odc6Image").alt;
 };
 
 function mp36Caption () {
-  var mp36Image = document.getElementById("mp36Image");
-  var mp36Caption = document.getElementById("mp36Caption");
-    mp36Caption.innerHTML = mp36Image.alt;
+  document.getElementById("mp36Caption").innerHTML = document.getElementById("mp36Image").alt;
 };
 
 function pm412Caption () {
-  var pm412Image = document.getElementById("pm412Image");
-  var pm412Caption = document.getElementById("pm412Caption");
-    pm412Caption.innerHTML = pm412Image.alt;
+  document.getElementById("pm412Caption").innerHTML = document.getElementById("pm412Image").alt;
 };
 
 function pm612Caption () {
-  var pm612Image = document.getElementById("pm612Image");
-  var pm612Caption = document.getElementById("pm612Caption");
-    pm612Caption.innerHTML = pm612Image.alt;
+  document.getElementById("pm612Caption").innerHTML = document.getElementById("pm612Image").alt;
 };
 
 function hpm612Caption () {
-  var hpm612Image = document.getElementById("hpm612Image");
-  var hpm612Caption = document.getElementById("hpm612Caption");
-    hpm612Caption.innerHTML = hpm612Image.alt;
+  document.getElementById("hpm612Caption").innerHTML = document.getElementById("hpm612Image").alt;
 };
 
 function ic1006Caption () {
-  var ic1006Image = document.getElementById("ic1006Image");
-  var ic1006Caption = document.getElementById("ic1006Caption");
-    ic1006Caption.innerHTML = ic1006Image.alt;
+  document.getElementById("ic1006Caption").innerHTML = document.getElementById("ic1006Image").alt;
 };
 
 function ic101018Caption () {
-  var ic101018Image = document.getElementById("ic101018Image");
-  var ic101018Caption = document.getElementById("ic101018Caption");
-    ic101018Caption.innerHTML = ic101018Image.alt;
+  document.getElementById("ic101018Caption").innerHTML = document.getElementById("ic101018Image").alt;
 };
