029ab5dcd996111221475f827c86a96ee8904e85
Yobo <yobany.perez@fisherathletic.com>
2020-01-28 15:14:55 -0500

    Scroll button prototype.

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,6 +204,26 @@ document.getElementById("windscreen-page-link").addEventListener("click", windsc
 document.getElementById("strength-page-link").addEventListener("click", strengthGridClick);
 document.getElementById("gym-page-link").addEventListener("click", gymGridClick);
 
+// Scroll button
+mybutton = document.getElementById("scroll-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // store the input search bar as a variable
 var searchInputBar = document.getElementById("searchInputBar");
 
