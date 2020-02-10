0f2d2fa5c0f0e56854113afa73213837b8027986
Yobo <yobany.perez@fisherathletic.com>
2020-02-10 09:22:41 -0500

    Updated styling and removed scrollToTop button from mobile view until bug is fixed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,11 +211,15 @@ mybutton = document.getElementById("scroll-button");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+  if(window.innerWidth > 550) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    };
   } else {
     mybutton.style.display = "none";
-  }
+  };
 }
 
 // When the user clicks on the button, scroll to the top of the document
