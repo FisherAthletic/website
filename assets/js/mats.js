fc29b07cb8050938b774839f0e8df640d0ac0de1
Yobo <yobany.perez@fisherathletic.com>
2020-05-19 14:20:52 -0400

    Update to mats page.

---
+++ assets/js/mats.js
@@ -0,0 +1,40 @@
+
+//products is coming from the file products.js
+let prodList = products
+
+let prodDiv = document.querySelector(".prod-card-grid")
+
+// creates new array with only elements that return true according to the over400 function
+let matProds = prodList.filter(item => { return item.category === "mats" })
+
+// map through the prodList array and for each item, run the following function
+// this will basically go through our list of products
+// and create a product card for each product and display it
+matProds.map(item => {
+    var div = document.createElement("div")
+    div.className = "prod-card"
+    var prodImage = document.createElement("img")
+    prodImage.setAttribute("src", item.image)
+    prodImage.setAttribute("alt", item.name)
+    prodImage.setAttribute("style", "width:100%")
+    var imagesBtn = document.createElement("button")
+    imagesBtn.innerHTML = `View more images <i class="fas fa-images"></i>`
+    imagesBtn.setAttribute("class", "prod-images-btn")
+    var prodTitle = document.createElement("h2")
+    prodTitle.innerHTML = item.name
+    var paragraph = document.createElement("p")
+    paragraph.innerHTML = `${item.description}`
+    var prodPrice = document.createElement("p")
+    prodPrice.innerHTML = `Price: $${item.price}`
+    prodPrice.className = "prod-card-price"
+    var quoteBtn = document.createElement("button")
+    quoteBtn.innerHTML = "Buy Now"
+    quoteBtn.setAttribute("class", "prod-quote-btn")
+    div.appendChild(prodImage)
+    div.appendChild(prodTitle)
+    div.appendChild(prodPrice)
+    div.appendChild(paragraph)
+    div.appendChild(imagesBtn)
+    div.appendChild(quoteBtn)
+    prodDiv.appendChild(div)
+})
--- assets/js/products.js
+++ assets/js/products.js
@@ -5,7 +5,7 @@ let products = [
         description: 'Tackle wheel teaches players to adjust their angle in pursuit. Teach open field tackling without bodies. Reduce injuries by eliminating repetitions. Teaches players to wrap up in open field. Made of 9lb. re-bond foam. 22 oz. coated vinyl in center and outside. 19 oz coated vinyl on faces of wheel.',
         category: 'football',
         subcategory: 'training',
-        price: 200,
+        price: 200.00,
         isAvailable: true,
         image: "https://lh3.googleusercontent.com/PViaBYBgCEYxZdawDtlPn2oNrDpOOrzCNczP73LCVUTpT_V0MaJyP8efGDlZzx9jYKHTmT0jwExv64jugIoor_KtTruw1RXVAVHOt186AE_olrth9jTASlTv_0_zE7s2Up7X1Shhsw=w600-h300-no"
     },{
@@ -14,7 +14,7 @@ let products = [
         description: 'Tackle wheel teaches players to adjust their angle in pursuit. Teach open field tackling without bodies. Reduce injuries by eliminating repetitions. Teaches players to wrap up in open field. Made of 9lb. re-bond foam. 22 oz. coated vinyl in center and outside. 19 oz coated vinyl on faces of wheel.',
         category: 'football',
         subcategory: 'training',
-        price: 300,
+        price: 300.00,
         isAvailable: true,
         image: "https://lh3.googleusercontent.com/tKlMhM0LHvRDQyGA1C_VAcHAEiR6TJA_7P1UtaqTGTjJRBxq-ntXqVDlNFXWKDvjw8EnAOszXvtT-G9KnsF2uw4zY4D31A1ru-M9O-PUD3IVdQ-9l6NzBwANiQRgcm9PsdZzQf8vwg=w600-h300-no"
     },{
@@ -23,7 +23,7 @@ let products = [
         description: 'Tackle wheel teaches players to adjust their angle in pursuit. Teach open field tackling without bodies. Reduce injuries by eliminating repetitions. Teaches players to wrap up in open field. Made of 9lb. re-bond foam. 22 oz. coated vinyl in center and outside. 19 oz coated vinyl on faces of wheel.',
         category: 'football',
         subcategory: 'training',
-        price: 400,
+        price: 400.00,
         isAvailable: true,
         image: "https://lh3.googleusercontent.com/wKRmBl1wG73B-IScguEKlV8Xim-ECszi9futJGty1DoxCxRFTfMXOiBBoM2MKweRJhMIgWYYrQiBnZ0e1K4vO4qKi7KkknJH5ehDXWk_6Ufmpgd0B0-4dAkoVyC2mknRa0d9Cv5Dig=w600-h300-no"
     },{
@@ -32,7 +32,7 @@ let products = [
         description: 'Tackle wheel teaches players to adjust their angle in pursuit. Teach open field tackling without bodies. Reduce injuries by eliminating repetitions. Teaches players to wrap up in open field. Made of 9lb. re-bond foam. 22 oz. coated vinyl in center and outside. 19 oz coated vinyl on faces of wheel.',
         category: 'football',
         subcategory: 'training',
-        price: 500,
+        price: 500.00,
         isAvailable: true,
         image: "https://lh3.googleusercontent.com/P2f6N-ODGsLmt_0ykIKmbq2yTen5QE1Sf4CnkhfjopqDqB-jFxGv7W0jRLcZZMjBS2cELgy8e57JvcTYa3nYixh23UmEtQaICT55T2VQ8doa8vuRSxYNPChrLtt3vYuoBaPhlCKeIw=w600-h300-no"
     },{
@@ -41,7 +41,7 @@ let products = [
         description: 'Tackle wheel teaches players to adjust their angle in pursuit. Teach open field tackling without bodies. Reduce injuries by eliminating repetitions. Teaches players to wrap up in open field. Made of 9lb. re-bond foam. 22 oz. coated vinyl in center and outside. 19 oz coated vinyl on faces of wheel.',
         category: 'football',
         subcategory: 'training',
-        price: 600,
+        price: 600.00,
         isAvailable: true,
         image: "https://lh3.googleusercontent.com/71Pf6cxln1lIOYdxwSnezKeWb5gjuSf_1bgFCet-RgjV2JbhDcVmYP1ArVxSNwYOCqU2BPb1M9kJZ8_XEowFTWcaBNrI_HpESl4Ku6aOns6fj5zT4ccwk6QBZ-NfRN_xp89bzQFvNw=w600-h300-no"
     },{
@@ -50,7 +50,34 @@ let products = [
         description: 'Tackle wheel teaches players to adjust their angle in pursuit. Teach open field tackling without bodies. Reduce injuries by eliminating repetitions. Teaches players to wrap up in open field. Made of 9lb. re-bond foam. 22 oz. coated vinyl in center and outside. 19 oz coated vinyl on faces of wheel.',
         category: 'football',
         subcategory: 'training',
-        price: 700,
+        price: 700.00,
+        isAvailable: true,
+        image: "https://lh3.googleusercontent.com/NAA0W6Kq72zhMUZn4BLRMf5E3kL2pDpWryXKr2slACvwf-H_Zqv4cjQ4evSxjCuIelJPXIEoEFuxgk3s01HUM1shWvHihdGzOxxjuSKbYySmTto3KfPul5vdBn6a2R2hTA1drTMweg=w600-h300-no"
+    },{
+        id: 'EXM124',
+        name: 'Economy Personal Exercise Mats',
+        description: 'One inch polyfoam covered with 18oz vinyl is used for this mat. The nonfolding design features a carrying handle on one long side. Stock color is royal. Additional colors available with orders of 6 or more.',
+        category: 'mats',
+        subcategory: 'personal',
+        price: 1125.00,
+        isAvailable: true,
+        image: "../assets/images/navbar/exm124-economy-personal-exercise-mat2.jpg"
+    },{
+        id: 'DEM24',
+        name: 'Non-Folding Personal Exercise Mats',
+        description: 'Two inch, high quality polyfoam is covered with easy-to-clean, long wearing, 18oz vinyl. Convenient carrying handles on one long side. Stock color is royal. Additional colors available with orders of 6 or more.',
+        category: 'mats',
+        subcategory: 'personal',
+        price: 1118.00,
+        isAvailable: true,
+        image: "../assets/images/navbar/Mats-DEM24-Non-Folding-Personal-Exercise-Mats.jpg"
+    },{
+        id: 'ITEMID',
+        name: 'Folding Rebond Mat',
+        description: 'All rebond foam mats are manufactured using 2" thick, 6lb density bonded foam and rugged 18oz coated vinyl. The 2" bonded foam filler gives more support than our urethane mats, but is slightly less firm than our polyethylene mats. This mat is a good choice for elementary school age children, seniors and for rehabilitation exercises. Specify color choice when ordering.',
+        category: 'mats',
+        subcategory: 'rebond',
+        price: 700.00,
         isAvailable: true,
         image: "https://lh3.googleusercontent.com/NAA0W6Kq72zhMUZn4BLRMf5E3kL2pDpWryXKr2slACvwf-H_Zqv4cjQ4evSxjCuIelJPXIEoEFuxgk3s01HUM1shWvHihdGzOxxjuSKbYySmTto3KfPul5vdBn6a2R2hTA1drTMweg=w600-h300-no"
     }
